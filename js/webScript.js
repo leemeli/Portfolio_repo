98eb0fefb37f3aea49b4e49767074645c48ff32a
Melody Lee <leemeli@uw.edu>
2016-10-30 17:49:55 -0700

    Complete home page

---
+++ js/webScript.js
@@ -0,0 +1,10 @@
+'use strict';
+
+$(document).ready(function() {
+    $("#frontTitle").animate({
+        opacity: 1
+     }, 1000, "swing");
+$("nav ul").animate({
+        opacity: 1
+     }, 1000, "swing");
+});
